dbe490c971e3e527ebdf5ea7d2524dc03966579c
Mike Ogrysko <michael.ogrysko@comcast.net>
2016-04-20 20:34:36 -0400

    added pitcher and rotation migrations

---
+++ migrations/20160404183902-create-pitcher.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Pitchers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      number: {
+        type: Sequelize.STRING
+      },
+      picture: {
+        type: Sequelize.STRING,
+        isUrl: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Games');
+  }
+};
